5ce2f9951c5425f06ef48ae182c333b7236e624f
daniel <dap5@cin.ufpe.br>
2022-10-09 19:23:59 -0300

    adding implementetion of analytics service methods and the analytics service tests

--- url_shortner/src/main/java/com/api/url_shortner/services/AnalyticsService.java
+++ url_shortner/src/main/java/com/api/url_shortner/services/AnalyticsService.java
@@ -1,5 +1,67 @@
 package com.api.url_shortner.services;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.url_shortner.collections.UrlMap;
+import com.api.url_shortner.models.GeneralAnalyticsResponse;
+import com.api.url_shortner.models.SpecificAnalyticsResponse;
+import com.api.url_shortner.repositories.UrlMapRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class AnalyticsService {
-    
+    @Autowired
+    private final UrlMapRepository urlMapRepository;
+
+    public GeneralAnalyticsResponse getGeneralAnalytics() {
+        GeneralAnalyticsResponse response;
+        UrlMap mostAccessed = urlMapRepository.findTopByOrderByNumberOfAccessesDesc();
+        UrlMap mostRequested = urlMapRepository.findTopByOrderByNumberOfCreationsRequestsDesc();
+        UrlMap oldestCreated = urlMapRepository.findTopByOrderByCreationDateAsc();
+        UrlMap lastUrlCreated = urlMapRepository.findTopByOrderByCreationDateDesc();
+        UrlMap lastAccessedUrl = urlMapRepository.findTopByOrderByLastAccessDateDesc();
+        UrlMap oldestAccessedUrl = urlMapRepository.findTopByOrderByLastAccessDateAsc();
+
+        
+        response = new GeneralAnalyticsResponse(10, 
+                mostRequested.getUrl(),
+                mostRequested.getNumberOfCreationsRequests(),
+                mostAccessed.getUrl(),
+                mostAccessed.getNumberOfAccesses(),
+                lastUrlCreated.getUrl(), 
+                lastUrlCreated.getCreationDate().toString(), 
+                oldestCreated.getUrl(),
+                oldestCreated.getCreationDate().toString(),
+                lastAccessedUrl.getUrl(),
+                lastAccessedUrl.getLastAccessDate().toString(),
+                oldestAccessedUrl.getUrl(),
+                oldestAccessedUrl.getLastAccessDate().toString());
+
+        return response;
+    }
+
+    public SpecificAnalyticsResponse getSpecificAnalytics(String input, boolean byShort) {
+        UrlMap urlMap;
+        if (byShort) {
+            urlMap = urlMapRepository.findByShortUrl(input).orElse(null);
+        } else {
+            urlMap = urlMapRepository.findByUrl(input).orElse(null);
+        }
+        if (urlMap == null) {
+            return null;
+        }
+        SpecificAnalyticsResponse response;
+        Duration duration = Duration.between(LocalDateTime.now(), urlMap.getCreationDate());
+        System.out.println(duration);
+        response = new SpecificAnalyticsResponse(urlMap.getCreationDate().toString(), duration.toString(),
+                urlMap.getNumberOfAccesses(), urlMap.getNumberOfCreationsRequests());
+        System.out.println(response);
+        return response;
+    }
 }
--- url_shortner/src/test/java/com/api/url_shortner/AnalyticsServiceTests.java
+++ url_shortner/src/test/java/com/api/url_shortner/AnalyticsServiceTests.java
@@ -1,6 +1,12 @@
 package com.api.url_shortner;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.api.url_shortner.collections.UrlMap;
+import com.api.url_shortner.models.GeneralAnalyticsResponse;
+import com.api.url_shortner.models.SpecificAnalyticsResponse;
 import com.api.url_shortner.repositories.UrlMapRepository;
 import com.api.url_shortner.services.AnalyticsService;
+import static org.mockito.Mockito.when;
 
 public class AnalyticsServiceTests {
 
@@ -19,7 +29,7 @@ public class AnalyticsServiceTests {
     AnalyticsService analyticsService;
 
     @Mock
-    UrlMapRepository urlRepository;
+    UrlMapRepository urlMapRepository;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -27,30 +37,79 @@ public class AnalyticsServiceTests {
     }
 
     @Test
-    public void test_getGeneralAnalytics_from_a_url_present_in_the_database(){
-        fail("not implemented");
+    public void test_getGeneralAnalytics_from_a_url_present_in_the_database() throws Exception{
+        UrlMap mockMostAccessed = new UrlMap("teste1", "teste1", LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(1), 1, 1);
+        UrlMap mockMostRequested = new UrlMap("teste2", "teste2", LocalDateTime.now().plusHours(2), LocalDateTime.now().plusHours(2), 2, 2);
+        UrlMap mockOldestCreated = new UrlMap("teste3", "teste3", LocalDateTime.now().plusHours(3), LocalDateTime.now().plusHours(3), 3, 3);
+        UrlMap mockLastUrlCreated = new UrlMap("teste4", "teste4", LocalDateTime.now().plusHours(4), LocalDateTime.now().plusHours(4), 4, 4);
+        UrlMap mockLastAccessedUrl = new UrlMap("teste5", "teste5", LocalDateTime.now().plusHours(5), LocalDateTime.now().plusHours(5), 5, 5);
+        UrlMap mockOldestAccessedUrl = new UrlMap("teste6", "teste6", LocalDateTime.now().plusHours(6), LocalDateTime.now().plusHours(6), 6, 6);
+        
+        when(urlMapRepository.findTopByOrderByNumberOfAccessesDesc()).thenReturn(mockMostAccessed);
+        when(urlMapRepository.findTopByOrderByNumberOfCreationsRequestsDesc()).thenReturn(mockMostRequested);
+        when(urlMapRepository.findTopByOrderByCreationDateAsc()).thenReturn(mockOldestCreated);
+        when(urlMapRepository.findTopByOrderByCreationDateDesc()).thenReturn(mockLastUrlCreated);
+        when(urlMapRepository.findTopByOrderByLastAccessDateDesc()).thenReturn(mockLastAccessedUrl);
+        when(urlMapRepository.findTopByOrderByLastAccessDateAsc()).thenReturn(mockOldestAccessedUrl);
+        
+        GeneralAnalyticsResponse result = analyticsService.getGeneralAnalytics();
+        
+        assertNotNull(result);
+        assertEquals(10, result.getNumberOfUrlMaps());
+        assertEquals(mockMostRequested.getUrl(), result.getMostRequestedUrl());
+        assertEquals(mockMostRequested.getNumberOfCreationsRequests(), result.getMostRequestedNumberOfRequests());
+        assertEquals(mockMostAccessed.getUrl(), result.getMostAccessedUrl());
+        assertEquals(mockMostAccessed.getNumberOfAccesses(), result.getMostAccessedNumberOfRequests());
+        assertEquals(mockLastUrlCreated.getUrl(), result.getLastUrlCreated());
+        assertEquals(mockLastUrlCreated.getCreationDate().toString(), result.getLastUrlCreatedDateTime());
+        assertEquals(mockOldestCreated.getUrl(), result.getOldestUrlCreated());
+        assertEquals(mockOldestCreated.getCreationDate().toString(), result.getOldestUrlCreatedDateTime());
+        assertEquals(mockLastAccessedUrl.getUrl(), result.getLastAccessedUrl());
+        assertEquals(mockLastAccessedUrl.getLastAccessDate().toString(), result.getLastAccessedUrlDateTime());
+        assertEquals(mockOldestAccessedUrl.getUrl(), result.getOldestAccessedUrl());
+        assertEquals(mockOldestAccessedUrl.getLastAccessDate().toString(), result.getOldestAccessedUrlDateTime());
+
+        closeable.close();
     }
 
     @Test
     public void test_getSpecifiAnalytics_from_a_url_present_in_the_database(){
-        fail("not implemented");
+        UrlMap mockUrlMap = new UrlMap("teste", "teste", LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(1), 1, 1);
+        when(urlMapRepository.findByUrl(anyString())).thenReturn(Optional.of(mockUrlMap));
+        SpecificAnalyticsResponse response = analyticsService.getSpecificAnalytics(anyString(), false);
+
+        assertNotNull(response);
+        assertEquals(mockUrlMap.getCreationDate().toString(), response.getCreationDateTime());
+        assertEquals(mockUrlMap.getNumberOfAccesses(), response.getNumberOfAccesses());
+        assertEquals(mockUrlMap.getNumberOfCreationsRequests(), response.getNumberOfCreationRequests());
     }
 
     @Test
     public void test_getSpecifiAnalytics_from_a_url_not_present_in_the_database(){
-        fail("not implemented");
+        when(urlMapRepository.findByUrl(anyString())).thenReturn(Optional.empty());
+        SpecificAnalyticsResponse response = analyticsService.getSpecificAnalytics(anyString(), false);
 
+        assertNull(response);
     }
 
     @Test
     public void test_getSpecifiAnalytics_from_a_shortUrl_present_in_the_database(){
-        fail("not implemented");
+        UrlMap mockUrlMap = new UrlMap("teste", "teste", LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(1), 1, 1);
+        when(urlMapRepository.findByShortUrl(anyString())).thenReturn(Optional.of(mockUrlMap));
+        SpecificAnalyticsResponse response = analyticsService.getSpecificAnalytics(anyString(), true);
 
+        assertNotNull(response);
+        assertEquals(mockUrlMap.getCreationDate().toString(), response.getCreationDateTime());
+        assertEquals(mockUrlMap.getNumberOfAccesses(), response.getNumberOfAccesses());
+        assertEquals(mockUrlMap.getNumberOfCreationsRequests(), response.getNumberOfCreationRequests());
     }
 
     @Test
     public void test_getSpecifiAnalytics_from_a_shortUrl_not_present_in_the_database(){
-        fail("not implemented");
+        when(urlMapRepository.findByUrl(anyString())).thenReturn(Optional.empty());
+        SpecificAnalyticsResponse response = analyticsService.getSpecificAnalytics(anyString(), true);
+
+        assertNull(response);
 
     }
 }
